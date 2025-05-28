d8a8aec7fe0cc064d8eea426bdc504c88d866151
Navnath Kadam <navnathkadam284@gmail.com>
2025-05-28 11:59:46 +0000

    Refactor comments to clarify React element rendering process and improve code documentation

--- custom-react.js
+++ custom-react.js
@@ -5,18 +5,29 @@
 // Babel or ESBuild like transpilers converts <m /> to that object like reactElement
 
 // Ex -> 
-//  import React from "react";
-//  React.createElement(
-//          'a',
-//          {href : "https://google.com" , target : "_blank" , style : "color : red;"},
-//          "This is My Text"
-//  )
+/* 
+    So we write like return in React 
+    <> 
+        <a href="https://google.com" target:"_blank" style : "color:red;"> 
+            Go to Google 
+        </a> 
+    </>
+    Babel for webpack or ESBuild for vite converts that into below 
+
+    import React from "react";
+    React.createElement(
+            'a',
+            {href : "https://google.com" , target : "_blank" , style : "color : red;"},
+            Go to Google
+        )
+*/
 
 
 // Interview Point of View 
 // {} inside react element is called as evaluated expression
 // Inside {} we can only declare evaluation , can't like if() else
 // why ? because in the end of day that variables are stored in that place where it is in object 
+
 
 
 function myCustomRender(reactElement , mainDiv){

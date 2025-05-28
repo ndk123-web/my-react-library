f5e9995b8f244a2ab85965a045d106cfd5a5d702
Navnath Kadam <navnathkadam284@gmail.com>
2025-05-28 11:26:44 +0000

    Enhance documentation with additional comments on React element structure and rendering process

--- custom-react.js
+++ custom-react.js
@@ -1,6 +1,24 @@
 // The Problem was what if there multiple attributes of element
 // solution used -> for in loop because key value object
 
+// in react internally also uses this reactElement object structure 
+// Babel or ESBuild like transpilers converts <m /> to that object like reactElement
+
+// Ex -> 
+//  import React from "react";
+//  React.createElement(
+//          'a',
+//          {href : "https://google.com" , target : "_blank" , style : "color : red;"},
+//          "This is My Text"
+//  )
+
+
+// Interview Point of View 
+// {} inside react element is called as evaluated expression
+// Inside {} we can only declare evaluation , can't like if() else
+// why ? because in the end of day that variables are stored in that place where it is in object 
+
+
 function myCustomRender(reactElement , mainDiv){
 
     const myElement = document.createElement(reactElement.type);
